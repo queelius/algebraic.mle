581dbcf4806f4ff37d978d9fd4295d013279c840
Alex Towell <queelius@gmail.com>
2022-01-31 12:59:12 -0600

    updates

--- R/fisher_scoring.R
+++ R/fisher_scoring.R
@@ -12,13 +12,13 @@ fisher_scoring <- function(theta0,info,score,eps=1e-5,max_iter=250L)
     nfo <- NULL
     sigma <- NULL
 
-    for (iter in 1:max_iter)
+    for (iter in 1L:max_iter)
     {
         nfo <- info(theta0)
         sigma <- MASS::ginv(nfo)
         s <- score(theta0)
         theta1 <- theta0 + sigma %*% s
-        if (max(abs(theta1-theta0)) < eps)
+        if (max(abs(s)) < eps)
             break
         theta0 <- theta1
     }
@@ -41,52 +41,27 @@ fisher_scoring <- function(theta0,info,score,eps=1e-5,max_iter=250L)
 #' @param loglike log-likelihood function
 #' @param score score function of type \eqn{R^p -> R^p}
 #' @param eps stopping condition
-#' @param normalize whether to normalize the jumps to a magnitude of max_alpha
-#' @param max_alpha maximum alpha step
-#' @param randomize randomize steps according to some energy function that
-#'                  decreases over time
+#' @param max_iter maximum iterations
 #' @export
 gradient_ascent <- function(theta0,
                             loglike,
                             score=NULL,
                             eps=1e-3,
-                            normalize=F,
-                            max_alpha=3,
-                            randomize=F)
+                            max_iter=250L)
 {
     if (is.null(score))
         score <- function(theta) numDeriv::grad(loglike,theta)
 
-    energy <- function(iter) { exp(-iter) }
-    norm <- function(x) { x / sqrt(sum(x^2)) }
-    ls <- function(t,p) { loglike(theta0 + t * p) }
-    #stop_cond <- function(s) { sqrt(sum(s^2)) < eps }
-    stop_cond <- function(s) { max(abs(s)) < eps }
-    proj <- function(theta)
-    {
-        for (j in 1:length(theta))
-        {
-            if (theta[j] < 0)
-                theta[j] <- stats::runif(1,1,5)
-        }
-        theta
-    }
-
-    i <- 1
+    i <- 1L
     repeat
     {
-        p <- score(theta0)
-        #if (stop_cond(p))
-
-        #if (randomize) p <- p + energy(i)*stats::rnorm(n=length(theta0))
-        #if (normalize) p <- norm(p)
-
-        theta1 <- proj(theta0 + p)
-        if (stop_cond(theta0-theta1))
+        s <- score(theta0)
+        theta1 <- theta0 + s
+        if (i == max_iter || max(abs(s) < eps))
         {
             structure(list(
                 theta.hat=theta0,
-                score=p,
+                score=s,
                 eps=eps,
                 iter=i),
                 class=c("mle_numerical","mle","estimate"))
@@ -94,7 +69,7 @@ gradient_ascent <- function(theta0,
 
 
         #alpha <- stats::optimise(ls,c(0,max_alpha),maximum=T,p=p)$maximum
-        #theta0 <- proj(theta0 + alpha * p)
-        i <- i + 1
+        theta0 <- theta1
+        i <- i + 1L
     }
 }
