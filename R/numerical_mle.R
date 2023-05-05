e4db2eee60f9559d113ce90b3a96f470f9c1deff
queelius <queelius@gmail.com>
2023-05-05 03:14:52 -0500

    some functions have too many options, changing it to accepting optional arguments as a list

--- R/numerical_mle.R
+++ R/numerical_mle.R
@@ -15,72 +15,75 @@
 #' 
 #' @param f Objective function to maximize, `f : R^d -> R`
 #' @param x0 Initial guess
-#' @param t_init Initial temperature
-#' @param t_end Final temperature
-#' @param sup Support function, returns TRUE if x is in the domain of f
-#' @param neigh Neighborhood function, returns a random neighbor of x
-#' @param max_iter Maximum number of iterations, used instead of t_end
-#'                 if not NULL, defaults to NULL
-#' @param iter_per_temp Number of iterations per temperature
-#' @param ... Additional arguments to neigh
-#' @param alpha Cooling factor
-#' @param debug If TRUE, print debugging information to the console
-#' @param trace If TRUE, track the history of positions and values
+#' @param options List of options
+#' * `t_init` Initial temperature
+#' * `t_end` Final temperature
+#' * `alpha` Cooling factor
+#' * `iter_per_temp` Number of iterations per temperature
+#' * `max_iter` Maximum number of iterations, used instead of t_end
+#'             if not NULL, defaults to NULL
+#' * `debug` If TRUE, print debugging information to the console
+#' * `trace` If TRUE, track the history of positions and values
+#' * `sup` Support function, returns TRUE if x is in the domain of f
+#' * `neigh` Neighborhood function, returns a random neighbor of x
+#' * `...` Additional arguments to neigh
 #' @return list with best solution (argmax) and its corresponding
-#'         objective function value (max), and optionally trace_x and trace_fx
+#'         objective function value (max), and optionally path
+#' @importFrom stats runif
 #' @export
-sim_anneal <- function(
-    f,
-    x0,
-    t_init = 100,
-    t_end = 1e-3,
-    alpha = 0.99,
-    iter_per_temp = 100,
-    sup = function(x) TRUE,
-    neigh = function(x) x + rnorm(length(x)),
-    max_iter = NULL,
-    debug = FALSE,
-    trace = FALSE, ...) {
-    stopifnot(is.function(f), is.function(sup), is.function(neigh),
-                t_init > 0, alpha > 0, alpha < 1, iter_per_temp > 0,
-                is.logical(debug), is.logical(trace), sup(x0))
+sim_anneal <- function(f, x0, options=list(), ...) {
+    defaults <- list(
+        t_init = 100,
+        t_end = 1e-6,
+        alpha = 0.95,
+        iter_per_temp = 100,
+        max_iter = 1e20,
+        debug = FALSE,
+        trace = FALSE,
+        sup = function(x) TRUE,
+        neigh = function(x) x + rnorm(length(x))
+    )
+    options <- modifyList(defaults, options)
+    stopifnot(options$t_init > 0,
+              options$alpha > 0, options$alpha < 1,
+              options$iter_per_temp > 0,
+              (is.null(options$max_iter) || options$max_iter > 0),
+              is.function(f),
+              is.function(options$neigh),
+              is.function(options$sup),
+              is.logical(options$debug),
+              is.logical(options$trace))
 
-    if (!is.null(max_iter)) {
-        stopifnot(max_iter > 0)
-        if (!is.null(t_end)) {
-            warning("t_end is ignored when max_iter is not NULL")
-        }
-        t_end <- t_init * alpha^max_iter
+    if (!options$sup(x0)) {
+        stop("initial guess x0 not in support")
     }
-    stopifnot(t_end > 0, t_end < t_init)
 
     argmax <- x0
     fmax <- f(argmax)
     fx0 <- fmax
-    t <- t_init
+    t <- options$t_init
     iter <- 0L
-    trace_x <- matrix(nrow=0,ncol=length(x0))
-    trace_fx <- c()
+    path <- matrix(nrow=0,ncol=length(x0))
 
-    while (t > t_end) {
+    while (t > options$t_end) {
         iter <- iter + 1L
-        x <- neigh(x0,...)
-        if (!sup(x)) {
-            if (debug) {
+        x <- options$neigh(x0,...)
+        if (!options$sup(x)) {
+            if (options$debug) {
                 cat("x = (", x ,") not in support, skipping\n")
             }
             next
         }
         fx <- f(x)
         if (is.nan(fx)) {
-            if (debug) {
+            if (options$debug) {
                 cat("f(x=",x,") is NaN, skipping\n")
             }
             next
         }
 
         if (fx > fmax) {
-            if (debug) {
+            if (options$debug) {
                 cat("temp:", t, ", max:", fmax, ", argmax:", argmax, "\n")
             }
 
@@ -89,53 +92,56 @@ sim_anneal <- function(
         }
 
         if (exp((fx - fx0) / t) > runif(1)) {
-            if (debug) {
+            if (options$debug) {
                 cat("temp:", t, ", val:", fx, ", x:", x, "\n")
             }
             x0 <- x
             fx0 <- fx
-            if (trace)
-            {
-                trace_x <- rbind(trace_x, x)
-                trace_fx <- c(trace_fx, fx)
+            if (options$trace) {
+                path <- rbind(path, x)
             }
         }
-        if (iter %% iter_per_temp == 0) {
-            t <- t * alpha
+        if (iter %% options$iter_per_temp == 0) {
+            t <- t * options$alpha
         }
     }
 
-    list(argmax = argmax, max = fmax, trace_x = trace_x, trace_fx = trace_fx)
+    sol <- list(argmax = argmax, max = fmax, options = options)
+    if (options$trace) {
+        sol$path <- path
+    }
+    sol
 }
 
 #' mle_optim
 #'
 #' This function takes the output of `optim` and turns it into an `mle` object.
-#' @param res the output of `optim`
+#' 
+#' @param sol the output of `optim`
+#' @return a `numerical_mle` object, specialized for `optim` (stats package)
+#' solutions.
 #' @importFrom MASS ginv
 #' @export
-mle_optim <- function(res) {
+mle_optim <- function(sol) {
     sigma <- NULL
     info <- NULL
-    if (!is.null(res$hessian))
+    if (!is.null(sol$hessian))
     {
-        info <- -res$hessian
+        info <- -sol$hessian
         sigma <- ginv(info)
     }
-    theta.hat <- mle(theta.hat=res$par,
-        loglike=res$value,
+    mle.sol <- mle(
+        theta.hat=sol$par,
+        loglike=sol$value,
         score=NULL,
         sigma=sigma,
         info=info,
         obs=NULL,
         nobs=NULL,
         superclasses=c("mle_optim","numerical_mle"))
-    theta.hat$counts <- res$counts
-    theta.hat$convergence <- res$convergence
-    theta.hat$converged <- res$convergence == 0
-    theta.hat$message <- res$message
-    theta.hat$method <- res$method
-    theta.hat
+    mle.sol$converged <- sol$convergence == 0
+    mle.sol$optim_data <- sol
+    mle.sol
 }
 
 #' mle_local_search
@@ -144,62 +150,80 @@ mle_optim <- function(res) {
 #' an initial guess `theta0` that is near it. Use a global search method
 #' like `sim_anneal` to find a good initial guess.
 #' 
-#' General local search method. It doesn't do anything fancy, but it is
-#' more easily tweakable than `optim` in the `stats` package and can be
-#' made to generate results when `optim` fails.
-#' I use it for debugging and testing, but I also use it to implement
-#' in a more transparent way the local search methods for newton-raphson
-#' and gradient ascent.
-#'
-#' @param l function, log-likelihood function of type `R^p -> R`
-#' @param theta0 numeric, initial guess, a column vector of size `p`
-#' @param dir function, promising direction function of type `R^p -> R^p`
-#' @param sup predicate function, domain of support function of type
-#'            `R^p -> {TRUE,FALSE}` for the log-likelihood function `l`
-#' @param eta numeric, learning rate, defaults to 1
-#' @param max_iter integer, maximum number of iterations
-#' @param max_iter_ls integer, maximum number of iterations for the line search
-#' @param eps numeric, tolerance for convergence
-#' @param r numeric, backtracking line search parameter, defaults to 0.8
-#' @param proj function, projection function of type `R^p -> R^p`
-#' @param tol function, tolerance function of type `R^p -> R`
-#' @param debug logical, output debugging information if `TRUE`;
-#'              defaults to `FALSE`
-#' @param trace logical, if `TRUE` store the path of the search in the
-#'             `trace` attribute of the output
+#' @param ll function, log-likelihood function
+#' @param theta0 numeric, initial guess
+#' @param dir function, promising direction function
+#' @param options list, options for the local search:
+#' * `sup` predicate function, domain of support for log-likelihood
+#' * `eta` numeric, learning rate, defaults to 1
+#' * `max_iter` integer, maximum number of iterations, defaults to 1000
+#' * `max_iter_ls` integer, maximum number of iterations for the line search,
+#' defaults to 1000
+#' * `abs_tol` numeric, tolerance for convergence, defaults to NULL
+#' (use `rel_tol`)
+#' * `rel_tol` numeric, relative tolerance for convergence, defaults to 1e-5
+#' * `r` numeric, backtracking line search parameter, defaults to 0.8
+#' * `proj` function, projection function to enforce domain of support
+#' * `norm` function, we pass the difference of successive theta updates
+#' to the norm to check for convergence, defaults to the infinity norm.
+#' * `debug` logical, output debugging information if TRUE; default FALSE
+#' * `trace` logical, if TRUE store the path of the search in the `path`
+#' attribute of the output
+#' @return an `mle` object with the following additional attributes:
+#' * `iter` integer, the number of iterations
+#' * `converged` logical, whether the algorithm converged
+#' * `options` list, the options used for the search
+#' * `path` matrix, the path of the search if `trace` is TRUE
 #' @export
 mle_local_search <- function(
     ll,
-    theta0,
     dir,
-    eps = 1e-5,
-    proj = NULL,
-    sup = NULL,
-    tol = NULL,
-    eta = 1,
-    r = 0.8,
-    max_iter = 1000L,
-    max_iter_ls = 1000L,
-    debug = FALSE,
-    trace = FALSE) {
-    stopifnot(eps > 0, eps < 1, eta > 0, r > 0, r < 1,
-        max_iter > 0, max_iter_ls > 0,
-        is.function(ll), is.function(dir))
-    if (is.null(proj)) proj <- function(theta) theta
-    stopifnot(is.function(proj))
-    if (is.null(tol)) tol <- function(dtheta) abs(max(dtheta))
-    stopifnot(is.function(tol))
-    if (is.null(sup)) sup <- function(theta) TRUE
-    stopifnot(is.function(sup))
+    theta0,
+    options = list())
+{
+    defaults <- list(
+        abs_tol = NULL,
+        rel_tol = 1e-5,
+        proj = function(theta) theta,
+        sup = function(theta) TRUE,
+        norm = function(dtheta) max(abs(dtheta)),
+        eta = 1,
+        r = 0.8,
+        max_iter = 1000L,
+        max_iter_ls = 1000L,
+        debug = FALSE,
+        trace = FALSE
+    )
+
+    options <- modifyList(defaults, options)
+    stopifnot(options$eta > 0, options$r > 0, options$r < 1,
+              options$max_iter > 0, options$max_iter_ls > 0,
+              is.logical(options$debug),
+              is.logical(options$trace),
+              is.function(ll), is.function(dir),
+              is.function(options$proj),
+              is.function(options$norm),
+              is.function(options$sup))
+
+    if (is.null(options$rel_tol)) {
+        stopifnot(!is.null(options$abs_tol), options$abs_tol > 0)
+    }
+    else {
+        stopifnot(!is.null(options$rel_tol), options$rel_tol > 0)
+    }
 
     theta <- theta0
     max <- ll(theta0)
-    trace_out <- matrix(nrow=0, ncol=length(theta0))
-
-    for (iter in 1:max_iter)
+    converged <- FALSE
+    path <- NULL
+    if (options$trace) {
+        path <- matrix(nrow=options$max_iter, ncol=length(theta0))
+    }
+    iter <- 1L
+    repeat
     {
         d <- dir(theta0)
-        if (debug) {
+        if (options$debug) {
             cat("dir =", d, "theta0 =", theta0, ", loglike =", max, "\n")
         }
 
@@ -208,72 +232,88 @@ mle_local_search <- function(
             f = ll,
             dir = d,
             x0 = theta0,
-            max_step = eta,
-            sup = sup,
-            fix = proj,
-            debug = debug,
-            r = 0.8,
-            max_iter = max_iter_ls,
-        )
+            max_step = options$eta,
+            sup = options$sup,
+            fix = options$proj,
+            debug = options$debug,
+            r = options$r,
+            max_iter = options$max_iter_ls)
 
         theta <- res$argmax
         max <- res$max
 
         if (!res$found_better) {
+            if (options$debug) {
+                cat("backtracking line search failed to find a better solution\n")
+            }
             warning("backtracking line search failed to find a better solution")
             break
         }
-        if (trace) {
-            trace_out <- rbind(trace_out, theta)
+        if (options$trace) {
+            path[iter,] <- as.vector(theta)
         }
-        if (tol(theta - theta0) < eps) {
-            break
+
+        if (is.null(options$abs_tol)) {
+            if (options$norm(theta - theta0) < options$rel_tol * norm(theta)) {
+                converged <- TRUE
+                break
+            }            
+        }
+        else {
+            if (options$norm(theta - theta0) < options$abs_tol) {
+                converged <- TRUE
+                break
+            }
         }
         theta0 <- theta
+        iter <- iter + 1L
+        if (iter > options$max_iter) {
+            break
+        }
     }
 
-    theta.hat <- mle(
-        theta.hat = theta,
-        loglike = max,
-        superclasses = c("mle_local_search","numerical_mle"))
-    theta.hat$iter <- iter
-    theta.hat$converged <- iter < max_iter
-    theta.hat$learning_rate <- eta
-    theta.hat$trace <- trace_out
-    theta.hat
-}
+    if (iter == options$max_iter) {
+        if (options$debug) {
+            cat("maximum number of iterations reached\n")
+        }
+        warning("maximum number of iterations reached")
+    }
 
+    sol <- mle(theta.hat = theta, loglike = max,
+        superclasses = c("mle_local_search", "numerical_mle"))
+    sol$iter <- iter
+    sol$converged <- converged
+    sol$options <- options
+
+    if (options$trace) {
+        sol$path <- path[1:iter, , drop = FALSE]
+    }
+    sol
+}
 
 #' MLE method using the Newton-Raphson method
 #'
-#' @param ll log-likelihood function of type `R^p -> R`
-#' @param theta0 initial guess of theta with `p` components
-#' @param info FIM function of type `R^p -> R^{p-by-q}`.
-#' @param score score Function of type `R^p -> R^p`, the gradient of `ll``
-#' @param inverted if `TRUE` `info` is covariance instead of FIM
-#' @param eps numeric, stopping criterion
-#' @param sup domain of support function of type `R^p -> {T,F}` for `ll`
-#' @param eta numeric, learning rate, defaults to 1
-#' @param r backing tracking parameter
-#' @param max_iter maximum number of iterations
-#' @param debug logical, output debugging info if `TRUE`; defaults to `FALSE`
-#'
+#' @param ll log-likelihood function
+#' @param theta0 initial guess
+#' @param info FIM function
+#' @param score score Function, the gradient of log-likelihood
+#' @param inverted logical, if TRUE `info` is covariance instead of FIM
+#' @param options list, options for the local search, see `mle_local_search`
+#' @return an object of class `mle_newton_raphson`, which is an `mle` object
 #' @importFrom MASS ginv
 #' @export
-mle_newton_raphson <- function(ll,
-                               theta0,
-                               info,
-                               score,
-                               eps = 1e-5,
-                               sup = NULL,
-                               eta = 1,
-                               max_iter = 0L,
-                               r = 0.5,
-                               proj = NULL,
-                               tol = NULL,
-                               debug = FALSE,
-                               inverted = FALSE,
-                               trace = FALSE) {
+mle_newton_raphson <- function(
+    ll,
+    score,
+    info,
+    theta0,
+    inverted = FALSE,
+    options = list())
+{
+    stopifnot(is.function(ll),
+              is.function(info),
+              is.function(score))
+
     V <- NULL
     if (inverted) {
         V <- info
@@ -283,23 +323,17 @@ mle_newton_raphson <- function(ll,
         V <- function(theta) ginv(info(theta))
     }
     dir <- function(theta) V(theta) %*% score(theta)
-    res <- mle_local_search(
+    sol <- mle_local_search(
         ll = ll,
         theta0 = theta0,
         dir = dir,
-        eps = eps,
-        sup = sup,
-        eta = eta,
-        proj = proj,
-        tol = tol,
-        max_iter = max_iter,
-        debug = debug,
-        trace = trace)
-    class(res) <- c("mle_newton_raphson", class(res))
-    res$score <- score(res$theta.hat)
-    res$sigma <- V(res$theta.hat)
-    res$info <- info(res$theta)
-    res
+        options = options)
+
+    class(sol) <- c("mle_newton_raphson", class(sol))
+    sol$score <- score(sol$theta.hat)
+    sol$sigma <- V(sol$theta.hat)
+    sol$info <- info(sol$theta)
+    sol
 }
 
 #' mle_gradient_ascent
@@ -309,47 +343,20 @@ mle_newton_raphson <- function(ll,
 #' @param theta0 initial guess of theta with `p` components
 #' @param ll log-likelihood function
 #' @param score score function of type `R^p -> R^p`, the gradient of `ll`
-#' @param eps numeric, stopping criterion
-#' @param sup domain of support function of type `R^p -> {TRUE,FALSE}` for `ll`
-#' @param eta numeric, learning rate, defaults to 1
-#' @param r numeric, backing tracking parameter
-#' @param max_iter integer, maximum iterations
-#' @param tol tolerance function of type `R^p -> R`
-#' @param proj projection function of type `R^p -> R^p`
-#' @param debug logical, output debugging info if `TRUE`; defaults to `FALSE'
 #' @importFrom MASS ginv
 #' @importFrom numDeriv hessian
 #' @export
-mle_gradient_ascent <- function(ll,
-                                theta0,
-                                score,
-                                eps = 1e-5,
-                                sup = NULL,
-                                eta = 1,
-                                r = 0.5,
-                                tol = NULL,
-                                proj = NULL,
-                                max_iter = 0L,
-                                debug = FALSE,
-                                trace = FALSE) {
-    res <- mle_local_search(
+mle_gradient_ascent <- function(ll, theta0, score, options) {
+    sol <- mle_local_search(
         ll = ll,
         theta0 = theta0,
         dir = score,
-        eps = eps,
-        sup = sup,
-        tol = tol,
-        eta = eta,
-        r = r,
-        proj = proj,
-        max_iter = max_iter,
-        debug = debug,
-        trace = trace)
-    class(res) <- c("mle_gradient_ascent", class(res))
-    res$info <- -hessian(ll, res$theta.hat)
-    res$score <- score(res$theta.hat)
-    res$sigma <- ginv(res$info)
-    res
+        options = options)
+    class(sol) <- c("mle_gradient_ascent", class(sol))
+    sol$info <- -hessian(ll, sol$theta.hat)
+    sol$score <- score(sol$theta.hat)
+    sol$sigma <- ginv(sol$info)
+    sol
 }
 
 #' Function to determine whether an `mle` object has converged.
--- inst/scripts.R
+++ inst/scripts.R
@@ -1,7 +1,9 @@
 
 # store sequence of steps in gradient ascent/newton raphson and plot the points
 # overlay it with loglike
-
+library(tidyverse)
+library(md.tools)
+library(stats)
 
 theta <- c(100,2)
 n <- 17
@@ -11,213 +13,58 @@ scr <- weibull_shape_scale_score(data)
 nfo <- weibull_shape_scale_fim(data)
 
 theta0 <- c(5,15)
-k0 <- theta0[1]
 
 sup.weibull <- function(theta) {
-    k <- theta[1]
-    b <- theta[2]
-    if (k <= 0) {
-        return(FALSE)
-    }
-    if (b <= 0) {
-        return(FALSE)
-    }
-    TRUE
-}
-proj.weibull <- function(theta) {
-    k <- theta[1]
-    b <- theta[2]
-    if (k <= 0) {
-        k <- 1e-3
-    }
-    if (b <= 0) {
-        b <- 1e-3
-    }
-    c(k,b)
+    all(theta > 0)
 }
 
-#sim_anneal(
-#    f=loglik,
-#    x0=theta0,
-#    t_init=100,
-#    alpha=.99,
-#    sup=sup.weibull,
-#    iter_per_temp=100,
-#    t_end=1e-12,
-#    trace=TRUE,
-#    debug=FALSE)
+theta.start <- sim_anneal(
+    f=loglik,
+    x0=theta0,
+    options=list(
+        t_init=100,
+        t_end=1e-4,
+        alpha=0.99,
+        iter_per_temp=200,
+        sup=sup.weibull,
+        debug=FALSE,
+        trace=TRUE))
 
-optim(
-  par=theta0,
-  fn=ll.wei,
-  method="SANN",
-  control=list(maxit=100000,fnscale=-1,temp=100))$par[1]
-z <- theta.sa$trace_fx
-x <- theta.sa$trace_x[,1]
-y <- theta.sa$trace_x[,2]
-df <- tibble(x=x, y=y, z=z)
-
-threshold <- quantile(df$z, 0.05) # removes the lowest 5% of data
-df <- df[df$z > threshold, ]
-
-# let's plot the z vs iteration
-ggplot(df, aes(x=1:length(z), y=z)) +
-  geom_line() +
-  theme_minimal() +
-  xlab("iteration") +
-  ylab("loglike")
-
-# plot the heatmap
-library(ggplot2)
-ggplot(df, aes(x=x, y=y, color=z)) +
-  geom_point() +
-  theme_minimal() +
-    xlab("shape") +
-    ylab("scale") +
-    scale_color_gradient(low="blue", high="red")
+logliks <- apply(theta.start$path,1,loglik)
+plot(logliks,type="l",xlab="iteration",ylab="log-likelihood")
 
 theta.mle <- mle_weibull_shape_scale(
     data,
-    k0=theta.sa$argmax[1],
-    eps=1e-16)
-
-theta.ga <- mle_gradient_ascent(
-    ll=loglik,
-    theta0=theta.sa$argmax,
-    score=scr,
-    debug=FALSE,
-    proj=proj.weibull,
-    sup=sup.weibull,
-    eps=1e-16,
-    eta=1,
-    max_iter=1000L)
+    k0=theta.start$argmax[1],
+    eps=1e-10)
 
 theta.nr <- mle_newton_raphson(
     ll=loglik,
-    theta0=theta.sa$argmax,
+    theta0=theta.start$argmax,
     score=scr,
     info=nfo,
-    proj=proj.weibull,
-    sup=sup.weibull,
-    eps=1e-20,
-    eta=1,
-    max_iter=1000L,
-    debug=FALSE)
+    options=list(
+        sup=sup.weibull,
+        rel_tol=1e-12,
+        eta=.1,
+        trace=TRUE))
 
+trace.ll <- apply(theta.nr$trace,1,loglik)
+plot(trace.ll,type="l")
+
+point(theta.nr)
 mle_local_search
 
-
 theta.optim <- mle_optim(optim(
-    par=theta.sa$argmax,
+    par=theta.start$argmax,
     fn=loglik,
     gr=scr,      
     hessian=TRUE,
     control=list(fnscale=-1,reltol=1e-16, maxit=2000000)))
 
-options(digits=20)
-names(theta.sa$max) <- "sa"
-l.mle <- loglike(theta.mle)
-names(l.mle) <- "mle"
-l.optim <- loglike(theta.optim)
-names(l.optim) <- "optim"
-l.nr <- loglike(theta.nr)
-names(l.nr) <- "nr"
-l.ga <- loglike(theta.ga)
-names(l.ga) <- "ga"
-
-sort(c(theta.sa$max,l.mle,l.optim,l.nr,l.ga))
-
-##########
-# ggplot #
-##########
-
-library(ggplot2)
-
-delta <- 0.75
-diff <- delta / 100
-as <- seq(center[1]-delta/2,center[1]+delta/2,diff)
-bs <- seq(center[2]-delta/2,center[2]+delta/2,diff)
-
-df <- tibble(x=numeric(),y=numeric(),z=numeric())
-
-N <- length(as) * length(bs)
-t <- numeric(length=N)
-u <- numeric(length=N)
-v <- numeric(length=N)
-
-i <- 1L
-for (a in as)
-{
-    for (b in bs)
-    {
-        t[i] <- a
-        u[i] <- b
-        v[i] <- loglik(c(a,b))
-        i <- i + 1L
-    }
-}
-
-#sqrt(t(scr(center))%*%scr(center))
-
-d <- scr(center)
-mag <- sqrt(sum(d^2))
-d.begin <- center
-d.end <- center + .01*(d/mag)
 
 
-d2 <- MASS::ginv(nfo(center)) %*% scr(center)
-mag2 <- sqrt(sum(d2^2))
-d2.begin <- center
-d2.end <- center + .01*(d2/mag2)
-
-
-df <- tibble(x=t,y=u,z=v)
-ggplot(df, aes(x=x,y=y,z=z)) + geom_contour(bins=100,aes(colour=after_stat(level))) +
-    geom_point(aes(x=center[1],y=center[2]),colour="red") +
-    geom_segment(aes(x=d.begin[1],
-                     y=d.begin[2],
-                     xend=d.end[1],
-                     yend=d.end[2]),
-                 arrow = arrow(length = unit(0.2, "cm"))) +
-    geom_segment(aes(x=d.begin[1],
-                     y=d.begin[2],
-                     xend=d2.end[1],
-                     yend=d2.end[2]),
-                 arrow = arrow(length = unit(0.2, "cm")))
-##########
-# plotly #
-##########
-library(plotly)
-
-center <- point(good2)
-delta <- 1.5
-diff <- delta / 1000
-as <- seq(center[1]-delta/2,center[1]+delta/2,diff)
-bs <- seq(center[2]-delta/2,center[2]+delta/2,diff)
-zs <- matrix(nrow=length(as),ncol=length(bs))
-
-i <- 1L
-j <- 1L
-for (a in as)
-{
-    for (b in bs)
-    {
-        zs[i,j] <- loglik(c(a,b))
-        j <- j + 1L
-    }
-    j <- 1L
-    i <- i + 1L
-}
-
-plot_ly(x=as,y=bs,z=zs,type="surface")
-#plot_ly(x=as,y=bs,z=zs,type="contour")
-
-
-
-
-###
-
-
+#####################################
 # TODO: bias correction, using bootstrap: theta.bias_cor = theta.hat + boot_estimate(data,mle_solver)$bias.hat
 # we knwo true parameter since it's a sim, so evaluate performacne of bias corrected theta.bias_cor
 # versus pure MLE estimate theta.hat.
@@ -292,10 +139,7 @@ ggplot(df.tmp) +
 
 
 
-##
-
-
-
+## PIs
 n <- 100
 theta <- c(4,2)
 x <- rnorm(n,mean=theta[1],sd=sqrt(theta[2]))
